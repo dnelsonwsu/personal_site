3f01c1bf3f413575b64dd4f3a80f70726b1cb041
Derek <dnelsonwsu@gmail.com>
2015-08-10 22:58:11 -0600

    get s3 working

---
+++ django_site/s3utils/__init__.py
@@ -0,0 +1,7 @@
+__author__ = 'derek'
+
+
+from storages.backends.s3boto import S3BotoStorage
+
+StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
+MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
