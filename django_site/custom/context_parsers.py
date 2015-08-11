05f5dfb9172feb62f2d2d567632f829bf2ee50bb
Derek <dnelsonwsu@gmail.com>
2015-08-11 02:17:32 -0600

    randomize images

--- django_site/custom/context_parsers.py
+++ django_site/custom/context_parsers.py
@@ -2,10 +2,12 @@ __author__ = 'derek'
 
 from mezzanine.galleries.models import GalleryImage
 from mezzanine.galleries.models import Gallery
+import random
 
 RECENT_GALLERY_IMAGES_TO_SHOW = 10
 
 def all_gallery_images(request):
-    gallery_images = GalleryImage.objects.order_by('created')[:RECENT_GALLERY_IMAGES_TO_SHOW]
+    gallery_images = GalleryImage.objects.all().order_by('?')[:RECENT_GALLERY_IMAGES_TO_SHOW]
+    #gallery_images = GalleryImage.objects.order_by('created')[:RECENT_GALLERY_IMAGES_TO_SHOW]
 
     return {"all_gallery_images": gallery_images}
